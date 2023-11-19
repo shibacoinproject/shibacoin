840674cf1e05341d17e8efae35b3a70116dd58ac
shibacoinproject <97772225+shibacoinproject@users.noreply.github.com>
2023-11-19 08:29:39 +0700

    Update shibacoin-build-ubuntu.sh

--- shibacoin-build-ubuntu.sh
+++ shibacoin-build-ubuntu.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+# =============================================================================#
+# this script can use for Ubuntu 18.04 LTS and Ubuntu 20.04 LTS
+# recommended use linux fresh installer
+# copy this file into root folder
+# make to the executable file with command :
+# chmod +x shibacoin-build-ubuntu.sh 
+# ./shibacoin-build-ubuntu.sh
+# =============================================================================#
+
+# added swapfile ( you can change swapfile allocate in : fallocate -l 3G /swapfile
+
 swapoff -a
-fallocate -l 3G /swapfile  
+fallocate -l 2G /swapfile  
 chown root:root /swapfile  
 chmod 0600 /swapfile  
 sudo bash -c "echo 'vm.swappiness = 10' >> /etc/sysctl.conf"  
@@ -11,32 +22,69 @@ echo '/swapfile none swap sw 0 0' >> /etc/fstab
 free -m 
 df -h
 
-rm -rf ~/root/.shibacoin
-
+# Prepare to build, Update your Ubuntu server
 cd ~ && sudo apt-get update && sudo apt-get upgrade -y &&
-sudo apt-get install git curl cmake automake python3 bsdmainutils libtool autotools-dev libboost-all-dev libssl-dev libevent-dev libdb++-dev libminiupnpc-dev libnatpmp-dev systemtap-sdt-dev libprotobuf-dev protobuf-compiler libzmq3-dev libsqlite3-dev pkg-config net-tools build-essential -y &&
-sudo apt gen install sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools -y && 
-sudo apt install qtwayland5 -y && 
-sudo apt-get install libqrencode-dev -y && 
 
-sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' db-4.8.30.NC/dbinc/atomic.h
+# Install the required dependencies
+sudo apt-get install build-essential bsdmainutils libtool autotools-dev libboost-all-dev libssl-dev libevent-dev libprotobuf-dev protobuf-compiler pkg-config python3 -y &&
+
+sudo apt-get install cmake automake unzip net-tools -y &&
+
+# port UPnP
+sudo apt-get install libminiupnpc-dev libzmq3-dev -y &&
+
+# (provides ZMQ API 4.x)
+sudo apt-get install libzmq3-dev -y &&
+
+# Qt 5 with GUI
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler -y &&
+
+# libqrencode 
+sudo apt-get install libqrencode-dev -y &&
 
 # Install the repository ppa:bitcoin/bitcoin
 sudo apt-get install software-properties-common -y &&
-sudo apt-get update -y &&
+sudo add-apt-repository ppa:luke-jr/bitcoincore -y &&
+sudo apt update -y && 
+sudo apt upgrade -y
+
+# Download shibacoin on github 
 
 cd ~ 
+
+git clone -b master --single-branch https://github.com/shibacoinproject/shibacoin.git
+
 cd shibacoin
 
-./contrib/install_db4.sh `pwd`
+# Install libdb6.2 (Berkeley DB)
 
-export BDB_PREFIX='/root/shibacoin/db4'
-  
+BITCOIN_ROOT=$(pwd)
+
+# Pick some path to install BDB to, here we create a directory within the shibacoin directory
+BDB_PREFIX="${BITCOIN_ROOT}/build"
+mkdir -p $BDB_PREFIX
+
+# Fetch the source and verify that it is not tampered with
+wget 'http://download.oracle.com/berkeley-db/db-6.2.32.tar.gz'
+echo 'a9c5e2b004a5777aa03510cfe5cd766a4a3b777713406b02809c17c8e0e7a8fb  db-6.2.32.tar.gz' | sha256sum -c
+# -> db-6.2.32.tar.gz: OK
+tar -xzvf db-6.2.32.tar.gz
+
+# Build the library and install to our prefix
+cd db-6.2.32/build_unix/
+#  Note: Do a static build so that it can be embedded into the executable, instead of having to find a .so at runtime
+../dist/configure --enable-cxx --disable-shared --with-pic --prefix=$BDB_PREFIX
+make install
+
+# build and install shibacoin
+cd $BITCOIN_ROOT
+
 ./autogen.sh
 
-./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768" --enable-upnp-default --enable-hardening
+./configure --with-incompatible-bdb --enable-upnp-default LDFLAGS="-L${BDB_PREFIX}/lib/" CPPFLAGS="-I${BDB_PREFIX}/include/" CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768" --disable-tests --disable-bench --disable-gui-tests --disable-zmq --enable-hardening
 
-make 
+make
+ 
 make install
 
 sudo ufw enable -y 
@@ -48,6 +96,8 @@ sudo mkdir ~/.shibacoin
 
 cat << "CONFIG" >> ~/.shibacoin/shibacoin.conf
 daemon=1
+txindex=1
+staking=1
 listen=1
 server=1
 rpcport=44556
@@ -56,9 +106,12 @@ rpcuser=SWmtLhap34m213BDaVWghQcRuMBwP4pdPM
 rpcpassword=c=SHIBA
 rpcconnect=127.0.0.1
 rpcallowip=127.0.0.1
-addnode=51.77.48.45:33445
-addnode=51.195.249.132:33445
-addnode=135.125.225.55:33445
+addnode=dnsseed1.shibamore.cloud
+addnode=dnsseed2.shibamore.cloud
+addnode=dnsseed3.shibamore.cloud
+addnode=dnsseed4.shibamore.cloud
+addnode=dnsseed5.shibamore.cloud
+addnode=dnsseed6.shibamore.cloud
 CONFIG
 
 chmod 700 ~/.shibacoin/shibacoin.conf
